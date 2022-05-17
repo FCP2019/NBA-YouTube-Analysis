3fbcdc7c489f2b4388fe8cb2882ec357b79bafd7
Chris Patton <104529081+FCP2019@users.noreply.github.com>
2022-05-16 17:56:17 -0700

    Add files via upload

---
+++ NBA YouTube Over:Under Index Viewership.sql
@@ -0,0 +1,5 @@
+SELECT c.ChannelName, v.Title,  v.Published_date, c.Views/c.NumVids as avgviews,  c.Subscribers, c.Views as totalchannelviews, v.views, v.views/c.Subscribers as viewspersubscriber, v.Likes+v.Comments as engagements,  (v.Likes+v.Comments)/c.Subscribers as engagementspersubscriber
+
+FROM channels as c
+join videos as v
+on c.ChannelName = v.Channel;
