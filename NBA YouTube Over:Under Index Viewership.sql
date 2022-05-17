2d985314feaf01196f04d226c136167ebb1b3139
Chris Patton <104529081+FCP2019@users.noreply.github.com>
2022-05-16 17:57:30 -0700

    Update NBA YouTube Over:Under Index Viewership.sql

--- NBA YouTube Over:Under Index Viewership.sql
+++ NBA YouTube Over:Under Index Viewership.sql
@@ -1,4 +1,13 @@
-SELECT c.ChannelName, v.Title,  v.Published_date, c.Views/c.NumVids as avgviews,  c.Subscribers, c.Views as totalchannelviews, v.views, v.views/c.Subscribers as viewspersubscriber, v.Likes+v.Comments as engagements,  (v.Likes+v.Comments)/c.Subscribers as engagementspersubscriber
+SELECT c.ChannelName, 
+       v.Title,  
+       v.Published_date, 
+       c.Views/c.NumVids as avgviews,  
+       c.Subscribers, 
+       c.Views as totalchannelviews, 
+       v.views, 
+       v.views/c.Subscribers as viewspersubscriber, 
+       v.Likes+v.Comments as engagements,  
+       (v.Likes+v.Comments)/c.Subscribers as engagementspersubscriber
 
 FROM channels as c
 join videos as v
